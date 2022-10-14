ea7d38a0d8bf7e4525b073efe61d71cc5f38fcc6
DerekHan <donglak.han@gmail.com>
2022-10-14 12:13:38 -0500

    create init & buttonClick functions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 
+function buttonClick () {
+    console.log("button clicked")
+}
+
+function init() {
+    const buttons = 
+    document.
+    querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+        buttonClick(event.target.innerText);
+    })
+}
 
 
+
+init ();
