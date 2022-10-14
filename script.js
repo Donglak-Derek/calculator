c0efedb921ab2b02d4baba4d93cb6e8ed262bc8a
DerekHan <donglak.han@gmail.com>
2022-10-14 11:52:53 -0500

    restart

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function writeNum (num) {
+    myform.answer.value += num;
+    console.log(myform.answer.value)
+}
 
+function clearAll () {
+    myform.answer.value = "";
+    console.log("clean")
+}
+
+function calculate () {
+    myform.answer.value = eval(myform.answer.value) //NEVER USE eval()!!!
+    console.log(myform.answer.value)
+}
