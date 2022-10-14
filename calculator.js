aa158537c7ccbf8c0486d365860230d20b62aa64
DerekHan <donglak.han@gmail.com>
2022-10-14 15:38:05 -0500

    event handling & UI behavior

--- calculator.js
+++ calculator.js
@@ -1,19 +1,57 @@
 let buffer = "0";
+// let screenNumber = screen.innerText;
+
+const screen = document.querySelector(".screen");
 
 
-function handleSymbol (symbol) {
-    console.log(symbol);
-}
-
-function handleNumber (number) {
-    console.log(number);
-}
 
 function buttonClick (value) {
     if (isNaN(parseInt(value))) {
         handleSymbol(value);
     } else {
         handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber (number) {
+    if (buffer === "0") {
+        buffer = number;
+
+    } else {
+        buffer += number;
+    }
+}
+
+function handleSymbol (symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = "0";
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case "=":
+            console.log("equals");
+            break;
+        case "÷" :
+        case "×" :
+        case "-" :
+        case "+" :
+           handleMath(symbol);
+            break;
+
+    }   
+}
+
+function handleMath (value) {
+    if (buffer === "0") {
+        //do nothing
+        return;
     }
 }
 
@@ -25,12 +63,13 @@ function init() {
     .addEventListener("click", function (event) {
         buttonClick(event.target.innerText);
     })
+    console.log("Hi");
 }
 
-//reder number
-
-function rerendering () {
-    document.querySelector(".screen").innerText = buffer;
+//reder screen
+function rerender() {
+    screen.innerText = buffer;
 }
+
 // should init init function
 init ();
